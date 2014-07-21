1c27e00aa99c481995e4ba6e1e11c883da5725a6
Drew Wilson <Andrew.Wilson@noblis.org>
2014-07-21 12:58:17 -0400

    Updating

---
+++ src/main/java/com/google/cloud/genomics/localrepo/vcf/VCFHeader.java
@@ -0,0 +1,89 @@
+package com.google.cloud.genomics.localrepo.vcf;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * The header record of a VCF file. All this is essentially is a list of sample IDs.
+ */
+public final class VCFHeader {
+
+  /**
+   * A builder class for {@code Header} objects.
+   */
+  public static final class Builder {
+
+    private final ImmutableList.Builder<String> sampleIds;
+
+    Builder(List<String> sampleIds) {
+      this.sampleIds = ImmutableList.<String>builder().addAll(sampleIds);
+    }
+
+    /**
+     * Add a new sample Id to the list of sample Ids in the header.
+     */
+    public Builder addSampleId(String sampleId) {
+      this.sampleIds.add(sampleId);
+      return this;
+    }
+
+    /**
+     * Build the header.
+     */
+    public VCFHeader build() {
+      return new VCFHeader(sampleIds.build());
+    }
+  }
+
+  /**
+   * Static factory method for the {@code Header.Builder} class.
+   */
+  public static Builder builder() {
+    return new Builder(Collections.<String>emptyList());
+  }
+
+  private final List<String> sampleIds;
+
+  VCFHeader(List<String> sampleIds) {
+    this.sampleIds = sampleIds;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return this == obj
+        || null != obj
+        && VCFHeader.class == obj.getClass()
+        && Objects.equals(sampleIds(), ((VCFHeader) obj).sampleIds());
+  }
+
+  /**
+   * Return the list of sample Ids.
+   */
+  public List<String> sampleIds() {
+    return sampleIds;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(sampleIds());
+  }
+
+  /**
+   * Convert this {@code Header} into its {@code Header.Builder} representation.
+   */
+  public Builder toBuilder() {
+    return new Builder(sampleIds());
+  }
+
+  @Override
+  public String toString() {
+    List<String> sampleIds = sampleIds();
+    return String.format(
+        "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO%s",
+        sampleIds.isEmpty() ? "" : String.format("\tFORMAT\t%s", Joiner.on('\t').join(sampleIds)));
+  }
+}
